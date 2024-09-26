1baccbb58f069f21fbe3a00cc46816d450f1001b
Nicolas JACOB-PERES <nicolasjp05@gmail.com>
2024-09-26 12:55:02 +0200

    project anim

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,15 @@
 import grainImage from '@/assets/images/grain.jpg';
-import { PropsWithChildren } from 'react';
+import { ComponentPropsWithoutRef, PropsWithChildren } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export const Card = ({ 
     className, 
-    children 
-}: PropsWithChildren<{ className?: string; }>) => {
+    children,
+    ...other 
+}: ComponentPropsWithoutRef<"div">) => {
     return (
-        <div className={twMerge("bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none", className)}>
+        <div className={twMerge("bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none", className)}
+        {...other}>
             <div className="absolute inset-0 -z-10 opacity-5" style={{
                 backgroundImage: `url(${grainImage.src})`,
             }}>
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -57,8 +57,14 @@ export const ProjectsSection = () => {
           description="See how I transformed concepts into engaging digital experiences." 
         />
         <div className="flex flex-col mt-10 md:mt-20 gap-20">
-          {portfolioProjects.map((project) => (
-            <Card key={project.title} className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20">
+          {portfolioProjects.map((project, projectIndex) => (
+            <Card 
+              key={project.title} 
+              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 sticky"
+              style={{
+                top: `calc(64px + ${projectIndex * 40}px`,
+              }}
+            >
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
                   <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
