b35188db941e28df432f8f669c4c3681031c8658
Nicolas JACOB-PERES <nicolasjp05@gmail.com>
2024-09-26 17:54:19 +0200

    tape animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,7 @@ const config: Config = {
       },
       animation:{
         'ping-large': 'ping-large 1s ease-in-out infinite',
+        'move-left': 'move-left 1s linear infinite',
       },
       keyframes: {
         'ping-large': {
@@ -35,7 +36,11 @@ const config: Config = {
             transform: 'scale(3)',
             opacity: '0'
           }
-        }
+        },
+        'move-left': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' }
+        },
       }
     },
   },
