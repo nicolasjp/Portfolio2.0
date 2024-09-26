512231b998f76a2147a6a8bbeaf1069d19eb2c0f
Nicolas JACOB-PERES <nicolasjp05@gmail.com>
2024-09-26 11:49:39 +0200

    footer section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,41 @@
+import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
+
+const footerLinks =[
+  {
+    title: 'Youtube',
+    href: '#',
+  },
+  {
+    title: 'Twitter',
+    href: '#',
+  },
+  {
+    title: 'Instagram',
+    href: '#',
+  },
+  {
+    title: 'LinkedIn',
+    href: '#',
+  },
+]
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer className='relative -z-10 overflow-x-clip'>
+      <div className='absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10'></div>
+      <div className="container">
+        <div className='border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8'>
+          <div className='text-white/40'>&copy; 2024. All rights reserved.</div>
+          <nav className='flex flex-col md:flex-row items-center gap-8'>
+            {footerLinks.map((link) => (
+              <a href="#" key={link.title} className='inline-flex items-center gap-1.5'>
+                <span className='font-semibold'>{link.title}</span>
+                <ArrowUpRightIcon className='size-4' />
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 };
