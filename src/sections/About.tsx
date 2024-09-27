921a6bf98fc65103ecb8a5bd01bc6cc7c98be264
Nicolas JACOB-PERES <nicolasjp05@gmail.com>
2024-09-27 16:32:09 +0200

    maj about

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -9,6 +9,26 @@ import CssIcon from "@/assets/icons/css3.svg";
 import ReactIcon from "@/assets/icons/react.svg";
 import ChromeIcon from "@/assets/icons/chrome.svg";
 import GithubIcon from "@/assets/icons/github.svg";
+import OracleIcon from "@/assets/icons/oracle.svg";
+import BlenderIcon from "@/assets/icons/blender.svg";
+import DockerIcon from "@/assets/icons/docker.svg";
+import FigmaIcon from "@/assets/icons/figma.svg";
+import HadoopIcon from "@/assets/icons/hadoop.svg";
+import JavaIcon from "@/assets/icons/java.svg";
+import KeycloakIcon from "@/assets/icons/keycloak.svg";
+import MongoDB from "@/assets/icons/leaf.svg";
+import Python from "@/assets/icons/python.svg";
+import RBACIcon from "@/assets/icons/rbac.svg";
+import SQLIcon from "@/assets/icons/serveur-sql.svg";
+import ShellIcon from "@/assets/icons/shell.svg";
+import Neo4jIcon from "@/assets/icons/neo4j.svg";
+import QlikIcon from "@/assets/icons/qlik.svg";
+import ManagementIcon from "@/assets/icons/management.svg";
+import GitIcon from "@/assets/icons/git.svg";
+import BabylonIcon from "@/assets/icons/babylon.svg";
+import TypeScriptIcon from "@/assets/icons/typescript.svg";
+import NodejsIcon from "@/assets/icons/nodejs.svg";
+
 import mapImage from "@/assets/images/map.png";
 import smileMemoji from "@/assets/images/memoji-smile.png";
 import { CardHeader } from "@/components/CardHeader";
@@ -18,47 +38,109 @@ import { useRef } from "react";
 
 const toolboxItems = [
   {
-    title: 'JavaScript',
-    iconType: JavascriptIcon ,
+    title: 'Oracle',
+    iconType: OracleIcon,
   },
   {
-    title: 'HMTL5',
-    iconType: HtmlIcon,
+    title: 'SQL',
+    iconType: SQLIcon,
   },
   {
-    title: 'CSS3',
-    iconType: CssIcon,
+    title: 'MongoDB',
+    iconType: MongoDB,
+  },
+  {
+    title: 'Neo4j',
+    iconType: Neo4jIcon,
+  },
+  {
+    title: 'Hadoop',
+    iconType: HadoopIcon,
+  },
+  {
+    title: 'Qlik',
+    iconType: QlikIcon,
+  },
+
+  {
+    title: 'Keycloak',
+    iconType: KeycloakIcon,
+  },
+  {
+    title: 'RBAC',
+    iconType: RBACIcon,
+  },
+  {
+    title: 'Python',
+    iconType: Python,
+  },
+  {
+    title: 'Shell',
+    iconType: ShellIcon,
+  },
+  {
+    title: 'Java',
+    iconType: JavaIcon,
+  },
+  {
+    title: 'TypeScript',
+    iconType: TypeScriptIcon,
+  },
+  //Nodejs
+  {
+    title: 'Node.js',
+    iconType: NodejsIcon,
   },
   {
     title: 'React',
     iconType: ReactIcon,
   },
   {
-    title: 'Chrome',
-    iconType: ChromeIcon,
+    title: 'Docker',
+    iconType: DockerIcon,
+  },
+  {
+    title: 'Babylon.js',
+    iconType: BabylonIcon,
+  },  
+  {
+    title: 'Blender',
+    iconType: BlenderIcon,
+  },
+  {
+    title: 'Git',
+    iconType: GitIcon,
   },
   {
     title: 'Github',
     iconType: GithubIcon,
   },
+  {
+    title: 'Figma',
+    iconType: FigmaIcon,
+  },
+  {
+    title: 'Management',
+    iconType: ManagementIcon,
+  },  
 ]
 
 const hobbies = [
   {
-    title: 'Painting',
-    emoji: '🎨',
+    title: 'Tennis',
+    emoji: '🎾',
     left: '5%',
     top: '5%',
   },
   {
-    title: 'Photography',
-    emoji: '📸',
-    left: '50%',
+    title: 'Running',
+    emoji: '🏃‍♂️',
+    left: '40%',
     top: '5%',
   },
   {
-    title: 'Hiking',
-    emoji: '🥾',
+    title: 'Piano',
+    emoji: '🎹',
     left: '35%',
     top: '40%',
   },
@@ -71,8 +153,8 @@ const hobbies = [
   {
     title: 'Music',
     emoji: '🎵',
-    left: '70%',
-    top: '45%',
+    left: '65%',
+    top: '42%',
   },
   {
     title: 'Fitness',
@@ -86,7 +168,24 @@ const hobbies = [
     left: '45%',
     top: '70%',
   },
-
+  {
+    title: 'Travel',
+    emoji: '✈️',
+    left: '77%',
+    top: '73%',
+  },
+  {
+    title: 'Skiing',
+    emoji: '⛷️',
+    left: '75%',
+    top: '-10%',
+  },
+  {
+    title: 'Stock market',
+    emoji: '📈',
+    left: '70%',
+    top: '15%',
+  }
 ]
 
 export const AboutSection = () => {
@@ -117,8 +216,8 @@ export const AboutSection = () => {
                 description="Explore the technologies and tools I use to craft exceptional digital experiences." 
                 className=""
               />
-              <ToolboxItems items={toolboxItems} className="" itemsWrapperClassName="animate-move-left [animation-duration:30s]" />
-              <ToolboxItems items={toolboxItems} className="mt-6" itemsWrapperClassName="animate-move-right [animation-duration:15s]" />
+              <ToolboxItems items={toolboxItems} className="" itemsWrapperClassName="animate-move-left [animation-duration:40s]" />
+              <ToolboxItems items={toolboxItems} className="mt-6" itemsWrapperClassName="animate-move-right [animation-duration:30s]" />
             </Card>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8"> 
@@ -138,7 +237,7 @@ export const AboutSection = () => {
                     drag
                     dragConstraints={constraintRef}
                   >
-                    <span className="font-medium text-gray-950">{hobby.title}</span>
+                    <span className="font-medium text-gray-950 overflow-hidden whitespace-nowrap">{hobby.title}</span>
                     <span>{hobby.emoji}</span>
                   </motion.div>
                 ))}
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -19,7 +19,7 @@ export const ContactSection = () => {
             <div>
               <h2 className="font-serif text-2xl md:text-3xl">Let's create something amazing together</h2>
               <p className="text-sm md:text-base mt-2">
-                Ready to bring your next project to life? Let's connect and discuss how I can help you achieve your goals.
+                Want to know more about my work or discuss an opportunity? Feel free to reach out.
               </p>
             </div>
             <div>
